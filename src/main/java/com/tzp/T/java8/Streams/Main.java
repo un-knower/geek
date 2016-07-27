a614b4a2a3aa0f817584c3008c3b4195163f6317
Mr丶言西早 <tzp_Bingo@foxmail.com>
2016-07-27 09:22:19 +0800

    Add

---
+++ src/main/java/com/tzp/T/java8/Streams/Main.java
@@ -0,0 +1,148 @@
+package com.tzp.T.java8.Streams;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * author i
+ * date 2016/7/26 14:42
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> stringCollection = new ArrayList<>();
+        stringCollection.add("ddd2");
+        stringCollection.add("aaa2");
+        stringCollection.add("bbb1");
+        stringCollection.add("aaa1");
+        stringCollection.add("bbb3");
+        stringCollection.add("ccc");
+        stringCollection.add("bbb2");
+        stringCollection.add("ddd1");
+
+
+        //Filter
+        stringCollection
+                .stream()
+                .filter((s) -> s.startsWith("a"))
+                .forEach(System.out::println);
+
+        //Sorted
+        stringCollection
+                .stream()
+                .sorted()
+                .filter((s) -> s.startsWith("a"))
+                .forEach(System.out::println);
+
+        //Match
+        boolean anyStartsWithA =
+                stringCollection
+                        .stream()
+                        .anyMatch((s) -> s.startsWith("a"));
+
+        System.out.println(anyStartsWithA);      // true
+
+        boolean allStartsWithA =
+                stringCollection
+                        .stream()
+                        .allMatch((s) -> s.startsWith("a"));
+
+        System.out.println(allStartsWithA);      // false
+
+        boolean noneStartsWithZ =
+                stringCollection
+                        .stream()
+                        .noneMatch((s) -> s.startsWith("z"));
+
+        System.out.println(noneStartsWithZ);      // true
+
+        //Count
+        long startsWithB =
+                stringCollection
+                        .stream()
+                        .filter((s) -> s.startsWith("b"))
+                        .count();
+
+        System.out.println(startsWithB);    // 3
+
+        //Reduce
+        Optional<String> reduced =
+                stringCollection
+                        .stream()
+                        .sorted()
+                        .reduce((s1, s2) -> s1 + "#" + s2);
+
+        reduced.ifPresent(System.out::println);
+
+        /**
+         * 并发流
+         */
+        int max = 1000000;
+        List<String> values = new ArrayList<>(max);
+        for (int i = 0; i < max; i++) {
+            UUID uuid = UUID.randomUUID();
+            values.add(uuid.toString());
+        }
+
+        //顺序排序
+        long t0 = System.nanoTime();
+
+        long count = values.stream().sorted().count();
+        System.out.println(count);
+
+        long t1 = System.nanoTime();
+
+        long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+        System.out.println(String.format("顺序排序 : %d ms", millis));
+
+        //并发排序
+        long t2 = System.nanoTime();
+
+        long count1 = values.parallelStream().sorted().count();
+        System.out.println(count1);
+
+        long t3 = System.nanoTime();
+
+        long millis1 = TimeUnit.NANOSECONDS.toMillis(t3 - t2);
+        System.out.println(String.format("并发排序 : %d ms", millis1));
+
+
+        /**
+         * Maps
+         */
+
+        Map<Integer, String> map = new HashMap<>();
+
+        for (int i = 0; i < 10; i++) {
+            map.putIfAbsent(i, "val" + i);
+        }
+        map.forEach((id, val) -> System.out.println(val));
+
+        map.computeIfPresent(3, (num, val) -> val + num);
+        map.get(3);             // val33
+
+        map.computeIfPresent(9, (num, val) -> null);
+        map.containsKey(9);     // false
+
+        map.computeIfAbsent(23, num -> "val" + num);
+        map.containsKey(23);    // true
+
+        map.computeIfAbsent(3, num -> "bam");
+        map.get(3);             // val33
+
+        map.remove(3, "val3");
+        map.get(3);             // val33
+
+        map.remove(3, "val33");
+        map.get(3);             // null
+
+        map.getOrDefault(42, "not found");  // not found
+
+        map.merge(9, "val9", (value, newValue) -> value.concat(newValue));
+        map.get(9);             // val9
+
+        map.merge(9, "concat", (value, newValue) -> value.concat(newValue));
+        map.get(9);             // val9concat
+    }
+
+}
